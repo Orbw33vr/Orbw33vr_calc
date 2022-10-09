6145aece3f02331719f3355a0adb2ac3ff3b5221
orbw33vr <d.martin147p@gmail.com>
2022-10-08 22:17:56 -0400

    Adding new love compatibility calculator

---
+++ js/loveCalc.js
@@ -0,0 +1,21 @@
+//pseudo RNG love calculator
+prompt("Enter your lovers full name.");
+prompt("Enter your ful name.")
+let c = Math.random();
+c *= 100;
+c = Math.floor(c);
+//some functions practice
+function notCompatible(){
+    alert("You two are "+ c +"% compatible.\nSorry you two aren't too compatible.");
+}
+function isCompatible(){
+    alert("You two are "+ c +"% compatible.\nYou two are compatible AF!");
+}
+// if else statement with proper indentations below.
+if (c <= 50){
+    notCompatible();
+} else{
+    if (c >= 51){
+        isCompatible();
+    }
+}
