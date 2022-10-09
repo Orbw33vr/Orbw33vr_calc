4622ec93b7ae00c06581ced5eba72b0b7a643183
orbw33vr <d.martin147p@gmail.com>
2022-10-08 21:36:08 -0400

    robot instructions and more function practice

---
+++ js/robotInstructions.js
@@ -0,0 +1,33 @@
+function getMilk(money, costPerBottle) {
+    //assigning money variable to function getMilk
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+    let bottlesOfMilk = Math.floor(money / costPerBottle);
+    //rounding down
+    console.log("buy "+ bottlesOfMilk + " bottles of milk")
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+    return money % costPerBottle;
+}
+function calcBottles(startingMoney, costPerBottle){
+    let numberOfBottles = Math.floor(startingMoney / costPerBottle);
+    return numberOfBottles;
+}
+function calcChange(startingAmount, costPerBottle){
+    let change = startingAmount % costPerBottle;
+}
+console.log("Hello master here is your "+ getMilk(18, 8) + " change.")
