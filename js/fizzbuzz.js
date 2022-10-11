2936d99e73bc69e87de636fc567da4165817a86d
orbw33vr <d.martin147p@gmail.com>
2022-10-10 20:52:29 -0400

    Adding a longer version of the FizzBuzz challenge with arrays

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -13,3 +13,27 @@ function exCute() {
   }
 }
 exCute();
+
+/* fizzbuzz pushing to array using if statements
+
+let output = [];
+let count = 1;
+function fizzBuzz (){
+
+  if (count % 15 === 0){
+    output.push("FizzBuzz");
+  }
+  else if (count % 5 === 0){
+    output.push("Buzz");
+  }
+  else if (count % 3 === 0 ){
+    output.push("Fizz");
+  } else {
+    output.push(count);
+  }
+  count++;
+  console.log(output);
+  }
+fizzBuzz();
+
+*/
