9078e4a9cf54621d63a98a37c4b5ae944138dd38
orbw33vr <d.martin147p@gmail.com>
2022-10-10 21:23:10 -0400

    So it makes sense when running in Chrome snippets.

--- js/arrays.js
+++ js/arrays.js
@@ -1,9 +1,16 @@
 // Array Practice
 
 //Choosing random object from an Array
+/*
+  let names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+  let randomName = names[Math.floor(Math.random() * names.length)];
+  console.log(randomName + " is going to buy lunch today!");
+*/
 
-function whosPaying(names) {
-  let name = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
-  let randomName = name[Math.floor(Math.random() * name.length)];
-  return randomName + " is going to buy lunch today!";
-}
+//Another way to do this. AKA Longer Verion
+  let names = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+  let numberOfPeople = names.length;
+  let randomPersonPosition = Math.floor(Math.random()  * numberOfPeople);
+  let randomPerson = names[randomPersonPosition];
+  console.log(randomPerson + " is going to pay for lunch today!");
+  //alert(randomPerson + " is going to pay for lunch today!");
